a9a1b7d8c2b9cb0622ab5650017433447caf12ea
edgelord <malte.dostal@gmail.com>
2019-01-07 19:52:55 +0100

    Revert "Trying the write access"
    This reverts commit 1d1d4e9ecb7de7f5367ecd6e6dd2d12db2e65c5e.

---
+++ SwingInsectGame/Assets/Scripts/RopeSystem.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSystem : MonoBehaviour {
+
+    public GameObject ropeHingeAnchor;
+    public DistanceJoint2D ropeJoint;
+    public Transform crosshair;
+    public SpriteRenderer crosshairSprite;
+    public playerMovement playerMovement;
+    private bool ropeAttached;
+    private Vector2 playerPosition;
+    private Rigidbody2D ropeHingeAnchorRb;
+    private SpriteRenderer ropeHingeAnchorSprite;
+
+
+    public LineRenderer ropeRenderer;
+    public LayerMask ropeLayerMask;
+    private float ropeMaxCastDistance = 20f;
+    private List<Vector2> ropePositions = new List<Vector2>();
+
+    void Awake()
+    {
+        // 2
+        ropeJoint.enabled = false;
+        playerPosition = transform.position;
+        ropeHingeAnchorRb = ropeHingeAnchor.GetComponent<Rigidbody2D>();
+        ropeHingeAnchorSprite = ropeHingeAnchor.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // 3
+        var worldMousePosition =
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        var facingDirection = worldMousePosition - transform.position;
+        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        if (aimAngle < 0f)
+        {
+            aimAngle = Mathf.PI * 2 + aimAngle;
+        }
+
+        // 4
+        var aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
+        // 5
+        playerPosition = transform.position;
+
+        // 6
+        if (!ropeAttached)
+        {
+            SetCrosshairPosition(aimAngle);
+        }
+        else
+        {
+            crosshairSprite.enabled = false;    
+        }
+
+        HandleInput(aimDirection);
+    }
+
+
+    private void SetCrosshairPosition(float aimAngle)
+    {
+        if (!crosshairSprite.enabled)
+        {
+            crosshairSprite.enabled = true;
+        }
+
+        var x = transform.position.x + 1f * Mathf.Cos(aimAngle);
+        var y = transform.position.y + 1f * Mathf.Sin(aimAngle);
+
+        var crossHairPosition = new Vector3(x, y, 0);
+        crosshair.transform.position = crossHairPosition;
+    }
+
+
+
+    private void HandleInput(Vector2 aimDirection)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // 2
+            if (ropeAttached) return;
+            ropeRenderer.enabled = true;
+
+            var hit = Physics2D.Raycast(playerPosition, aimDirection, ropeMaxCastDistance, ropeLayerMask);
+
+            // 3
+            if (hit.collider != null)
+            {
+                ropeAttached = true;
+                if (!ropePositions.Contains(hit.point))
+                {
+                    // 4
+                    // Jump slightly to distance the player a little from the ground after grappling to something.
+                    transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 2f), ForceMode2D.Impulse);
+                    ropePositions.Add(hit.point);
+                    ropeJoint.distance = Vector2.Distance(playerPosition, hit.point);
+                    ropeJoint.enabled = true;
+                    ropeHingeAnchorSprite.enabled = true;
+                }
+            }
+            // 5
+            else
+            {
+                ropeRenderer.enabled = false;
+                ropeAttached = false;
+                ropeJoint.enabled = false;
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            ResetRope();
+        }
+    }
+
+    // 6
+    private void ResetRope()
+    {
+        ropeJoint.enabled = false;
+        ropeAttached = false;
+        ropeRenderer.positionCount = 2;
+        ropeRenderer.SetPosition(0, transform.position);
+        ropeRenderer.SetPosition(1, transform.position);
+        ropePositions.Clear();
+        ropeHingeAnchorSprite.enabled = false;
+    }
+}
---
+++ SwingInsectGame/Assets/Scripts/grapplingHook.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grapplingHook : MonoBehaviour {
+
+    private DistanceJoint2D joint;
+    private Vector3 cursorPos;
+    private RaycastHit2D raycast;
+    public float maxDistance = 10f;
+    public LayerMask mask;
+    public LineRenderer line;
+
+
+	void Start () {
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        line.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("yes");
+            cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            cursorPos.z = 0;
+
+            raycast = Physics2D.Raycast(transform.position, cursorPos - transform.position, maxDistance, mask);
+            if (raycast.collider != null && raycast.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                joint.enabled = true;
+                Vector2 connectPoint = raycast.point - new Vector2(raycast.collider.transform.position.x, raycast.collider.transform.position.y);
+                connectPoint.x = connectPoint.x / raycast.collider.transform.localScale.x;
+                connectPoint.y = connectPoint.y / raycast.collider.transform.localScale.y;
+                Debug.Log(connectPoint);
+                joint.connectedAnchor = connectPoint;
+
+                joint.connectedBody = raycast.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.distance = Vector2.Distance(transform.position, raycast.point);
+
+                line.enabled = true;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, raycast.point);
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            line.SetPosition(0, transform.position);
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            joint.enabled = false;
+            line.enabled = false;
+        }
+	}
+}
