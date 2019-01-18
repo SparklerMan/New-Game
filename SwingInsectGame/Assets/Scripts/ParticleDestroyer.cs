eab1e9879df719d336270b59509b55c0b5514143
Viswaa <deathstrike925@gmail.com>
2019-01-18 22:03:45 +0400

    Adde new script

---
+++ SwingInsectGame/Assets/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroyer : MonoBehaviour
+{
+	public float time;
+
+	void Start () 
+	{
+		Destroy (gameObject,time);
+	}
+}
