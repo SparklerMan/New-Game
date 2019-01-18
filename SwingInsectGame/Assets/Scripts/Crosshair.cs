169f68dcc5987bceede38eae039d5baa444371d0
Viswaa <deathstrike925@gmail.com>
2019-01-18 23:23:26 +0400

    Awesome

--- SwingInsectGame/Assets/Scripts/Crosshair.cs
+++ SwingInsectGame/Assets/Scripts/Crosshair.cs
@@ -6,11 +6,14 @@ public class Crosshair : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = pos;
 		
 	}
 }
